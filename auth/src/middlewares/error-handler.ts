72f9858c4c8941a90a199ff976f7fc7908d8b00b
nursultan <nurs@nurs.com>
2022-04-03 23:28:01 +0600

    error handler middleware edited

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log('Something went wrong', err);
+  if (err instanceof RequestValidationError) {
+    console.log('handling as a request validation error');
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    console.log('handling error as db connection error');
+  }
   res.status(400).send({ message: err.message });
 };
