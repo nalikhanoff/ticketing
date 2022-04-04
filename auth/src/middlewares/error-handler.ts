f6511a480a66918581cd2e95e64a2b23dc862d32
nursultan <prouser.01@mail.ru>
2022-04-04 08:59:39 +0600

    error handler configured

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -5,11 +5,14 @@ import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof RequestValidationError) {
-    console.log('handling as a request validation error');
+    const formattedErrors = err.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+    return res.status(400).send({ errors: formattedErrors });
   }
 
   if (err instanceof DatabaseConnectionError) {
-    console.log('handling error as db connection error');
+    return res.status(500).send({ errors: [{ message: err.reason }] });
   }
   res.status(400).send({ message: err.message });
 };
