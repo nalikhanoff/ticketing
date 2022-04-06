1cb99014fd72bb8c1c394a6b694798f5382330f9
nursultan <prouser.01@mail.ru>
2022-04-06 10:34:07 +0600

    password hashing and comparing methods added

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+import { Password } from '../services/password';
+
 interface UserAttrs {
   email: string;
   password: string;
@@ -25,9 +27,14 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
-
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
 userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
-
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
