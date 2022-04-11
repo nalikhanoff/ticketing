404c58ce1e91b72df924e0cdfa9747768671bbca
nursultan <prouser.01@mail.ru>
2022-04-11 11:41:00 +0600

    signup done

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -16,17 +16,28 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
 
-  password: {
-    type: String,
-    required: true,
+    password: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    toJSON: {
+      transform(_, ret) {
+        ret.id = ret._id;
+        ['_id', 'password'].forEach((k) => delete ret[k]);
+      },
+      versionKey: false,
+    },
+  }
+);
 userSchema.pre('save', async function (done) {
   if (this.isModified('password')) {
     const hashed = await Password.toHash(this.get('password'));
