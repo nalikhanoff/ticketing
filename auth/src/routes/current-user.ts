9a3723929ea391689bdf1845a3fbf31b467175cf
nursultan <nurs@nurs.com>
2022-04-17 17:20:05 +0600

    common module tested

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,7 +1,6 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import { currentUser } from '../middlewares/current-user';
-import { requireAuth } from '../middlewares/require-auth';
+import { currentUser } from '@sgtickets/common';
+import { requireAuth } from '@sgtickets/common';
 
 const router = express.Router();
 
