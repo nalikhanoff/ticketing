97fc92859f59f4bbc9b968d4f6c65f30d3c5e806
nursultan <prouser.01@mail.ru>
2022-04-19 18:25:21 +0600

    tickets yaml file

---
+++ tickets/src/index.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) throw new Error('JWT KEY must be defined');
+  try {
+    await mongoose.connect('mongodb://tickets-mongo-srv:27017/auth');
+    console.log('Connected to db');
+  } catch (e) {
+    console.error(e);
+  }
+  app.listen(3000, () => console.log('Listening on port 3000!!!'));
+};
+
+start();
