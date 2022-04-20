1c4d07246b4ad4295d3972f880cf6a228be92f44
nursultan <prouser.01@mail.ru>
2022-04-20 18:31:55 +0600

    tests of tickets started

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,8 +3,9 @@ import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT KEY must be defined');
+  if (!process.env.MONGO_URI) throw new Error('MONGO URI must be defined');
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to db');
   } catch (e) {
     console.error(e);
--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
 
-import { errorHandler, NotFoundError } from '@sgtickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@sgtickets/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,7 +14,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
-
+app.use(currentUser);
+app.use(createTicketRouter);
 app.all('*', async () => {
   throw new NotFoundError();
 });
