7b694a15b12579a222f7c7c3c5ee340541b79a29
nursultan <nurs@nurs.com>
2022-05-26 12:20:18 +0600

    test suite starting

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+import { Order, OrderStatus } from './order';
+
 interface TicketAttrs {
   title: string;
   price: number;
@@ -8,6 +10,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -23,7 +26,7 @@ const ticketSchema = new mongoose.Schema(
     price: {
       type: Number,
       required: true,
-      min: 0
+      min: 0,
     },
   },
   {
@@ -37,6 +40,15 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.statics.build = (a: TicketAttrs) => new Ticket(a);
+ticketSchema.methods.reserved = async function () {
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [OrderStatus.Created, OrderStatus.AwaitingPayment, OrderStatus.Complete],
+    },
+  });
+  return !!existingOrder;
+};
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
