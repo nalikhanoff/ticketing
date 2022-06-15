96b7e38ddc78e4de37ff87008c4b636b10617b1d
nursultan <nalikhanoff@gmail.com>
2022-06-15 13:24:41 +0600

    version included into events

--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -5,6 +5,7 @@ export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   date: {
     id: string;
+    version: number;
     status: OrderStatus;
     userId: string;
     expiresAt: string;
