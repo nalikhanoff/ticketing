7627d2bb5956feda7c04c2b5cfc04c721eb729a7
nursultan <nalikhanoff@gmail.com>
2022-07-01 17:37:12 +0600

    expiration service delay

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,9 +1,16 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@sgtickets/common';
 import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../../queues/expiration-queue';
 import { queueGroupName } from './queue-group-name';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
-  onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log('Waiting this many ms to process the job: ', delay);
+    await expirationQueue.add({ orderId: data.id }, { delay });
+
+    msg.ack();
+  }
 }
