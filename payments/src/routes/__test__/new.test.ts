be35380b28d1146023885c9c1a7868af47c89564
nursultan <nalikhanoff@gmail.com>
2022-08-05 13:56:59 +0600

    payment model usage

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { Order, OrderStatus } from '../../models/order';
+import { Payment } from '../../models/payment';
 import { stripe } from '../../stripe';
 
 it('throws not found when purchasing an order that does not exist', async () => {
@@ -76,4 +77,9 @@ it('returns a 201 with valid inputs', async () => {
   const stripeCharge = stripeCharges.data.find((charge) => (charge.amount = price * 100));
   expect(stripeCharge).toBeDefined();
   expect(stripeCharge!.currency).toEqual('usd');
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id,
+  });
+  expect(payment).not.toBeNull();
 });
