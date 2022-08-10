25c72547738a4c844fe2b33cad6fbe32448107c2
nursultan <nalikhanoff@gmail.com>
2022-08-10 22:53:36 +0600

    fix simulation

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -5,7 +5,7 @@ it('responds with details about the current user', async () => {
   const authResponse = await request(app)
     .post('/api/users/signup')
     .send({ email: 'test@test.com', password: 'password' })
-    .expect(400);
+    .expect(201);
   const token = authResponse.get('Set-Cookie');
   const response = await request(app).get('/api/users/currentuser').set('Cookie', token).send().expect(200);
   expect(response.body.currentUser.email).toEqual('test@test.com');
