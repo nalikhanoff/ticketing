f39f5ecbfef52d58e1a6eb4a04d7a1faf634e0d0
nursultan <nalikhanoff@gmail.com>
2022-08-10 22:04:32 +0600

    add links to header

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,3 +1,4 @@
+import Router from 'next/router';
 import { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import useRequest from '../../hooks/use-request';
@@ -8,7 +9,7 @@ const OrderShow = ({ order, currentUser }) => {
     url: '/api/payments',
     method: 'post',
     body: { orderId: order.id },
-    onSuccess: (payment) => console.log(payment),
+    onSuccess: () => Router.push('/orders'),
   });
 
   useEffect(() => {
