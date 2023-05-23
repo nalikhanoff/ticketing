882d73a90a177f880c7ebac6ee70015c0c461e24
nursultan <nalikhanoff@gmail.com>
2023-05-23 18:49:36 +0600

    current user error

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -23,7 +23,7 @@ AppComponent.getInitialProps = async (appContext) => {
 
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx, client, data.currentUser);
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx, client, data?.currentUser);
   }
 
   return {
